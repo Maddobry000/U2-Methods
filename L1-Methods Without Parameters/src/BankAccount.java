b774c6c62fbbc54115df2343e2a1a38b26a99dc7
maddobry000 <Maddobry000@warren.k12.in.us>
2021-09-10 13:06:36 -0400

    The new notes

--- L1-Methods Without Parameters/src/BankAccount.java
+++ L1-Methods Without Parameters/src/BankAccount.java
@@ -1,18 +1,37 @@
+import javax.swing.*;
+
 public class BankAccount {
+
 
     //create a program which simulates a bank account
 
     public static void main(String[] args) {
-
+    deposit();
     }
 
     /*write a method to simulate a deposit.
     Prompt the user to enter their balance and the deposit amount,
     then display the new balance. */
+    public static void deposit() {
 
+        double balance = Double.parseDouble(JOptionPane.showInputDialog(" What is your current balance? "));
+        double deposit = Double.parseDouble(JOptionPane.showInputDialog(" What is your deposit"));
 
+        balance += deposit;
+        JOptionPane.showMessageDialog(null, " your new balance is : " + balance);
+    }
     /*write a method to simulate a withdrawal.
     Prompt the user to enter their balance the withdrawal amount,
     then display the new balance.
      */
-}
+
+    public static void withdrawal (){
+
+        double balance = Double.parseDouble(JOptionPane.showInputDialog(" What is your current balance? "));
+        double withdrawal = Double.parseDouble(JOptionPane.showInputDialog(" What is your withdrawal amount "));
+
+        balance -= withdrawal;
+        JOptionPane.showMessageDialog(null, " your new balance is : " + balance);
+
+    }
+    }
--- L1-Methods Without Parameters/src/CircleCalc.java
+++ L1-Methods Without Parameters/src/CircleCalc.java
@@ -1,8 +1,12 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class CircleCalc {
 
 
     public static void main(String[] args) {
-
+    area();
+    circumference();
         //call the area method
 
         //call the circumference method
@@ -11,7 +15,25 @@ public class CircleCalc {
 
     //write a method to calculate a circle's area
 
+public static void area(){
 
+    DecimalFormat round = new DecimalFormat("#.##");
+        double radius = Double.parseDouble(JOptionPane.showInputDialog(" what is radius"));
+
+
+        double area = Math.PI * Math.pow(radius, 2);
+        JOptionPane.showMessageDialog(null, "The area is " + round.format(area));
+}
     //write a method to calculate a circle's circumference
+    public static void circumference(){
+        DecimalFormat round = new DecimalFormat("#.##");
+        double radius = Double.parseDouble(JOptionPane.showInputDialog(" what is the radius"));
 
+
+    double circumference = 2 * Math.PI * radius;
+
+    JOptionPane.showMessageDialog(null, "The circumference is " + round.format(circumference));
+
+
+    }
 }
