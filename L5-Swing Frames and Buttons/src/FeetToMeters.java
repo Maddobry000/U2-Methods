01bb1f630c9d4d588d536ff9d86d394820959d19
maddobry000 <Maddobry000@warren.k12.in.us>
2021-09-28 13:14:04 -0400

    Feet to meters

--- L5-Swing Frames and Buttons/src/FeetToMeters.java
+++ L5-Swing Frames and Buttons/src/FeetToMeters.java
@@ -1,6 +1,59 @@
+import jdk.nashorn.internal.scripts.JO;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class FeetToMeters {
 
     /* Create a program with two buttons. Clicking on the first button will prompt the user to enter a distance in feet and
     convert it to meters. Clicking on the second button will prompt the user to enter a distance in meters and convert to feet.
      */
+   /* 1.Create GUI
+       2. get input, when a button is clicked
+       3/ The Math - 3.28 feet in a meter
+             F->M = Divide by 3.28
+             m->F = Multiply by 3.28
+             4. Display result, after each button click,after getting input
+    */
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame("Feet/Meters Conversion");
+        JPanel panel = new JPanel();
+        JButton feetToMetersBut = new JButton("Feet to Meters");
+        JButton metersToFeetBut = new JButton("Meters to Feet");
+
+        window.setSize(250, 100);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+        feetToMetersBut.addActionListener(new FeetToMeter());
+        metersToFeetBut.addActionListener(new MeterToFeet());
+
+        panel.add(feetToMetersBut);
+        panel.add(metersToFeetBut);
+        window.add(panel);
+
+        window.setVisible(true);
+    }
+
+    private static class FeetToMeter implements ActionListener {
+
+        public void actionPerformed(ActionEvent actionEvent) {
+
+            double feet = Double.parseDouble(JOptionPane.showInputDialog("Enter the number of feet: "));
+            double meters = feet / 3.28;
+            JOptionPane.showMessageDialog(null, meters + " meters. ");
+        }
+    }
+
+    private static class MeterToFeet implements ActionListener {
+
+        public void actionPerformed(ActionEvent actionEvent) {
+
+            double meters = Double.parseDouble(JOptionPane.showInputDialog("Enter the number of feet: "));
+            double feet = meters * 3.28;
+            JOptionPane.showMessageDialog(null, feet + " meters. ");
+        }
+    }
 }
