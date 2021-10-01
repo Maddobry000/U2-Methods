d467dde39efb71c005c7294c94f7ed755b06a637
maddobry000 <Maddobry000@warren.k12.in.us>
2021-10-01 12:47:46 -0400

    Feet to meters

--- L6-Swing Text/src/BasicCalculator.java
+++ L6-Swing Text/src/BasicCalculator.java
@@ -1,3 +1,8 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
 public class BasicCalculator {
 
     /* Create a calculator program with two text fields
@@ -8,4 +13,71 @@ public class BasicCalculator {
 
     NO POP-UPS
      */
+    static JTextField inputField1,inputField2;
+    static JLabel resultLabel;
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame("Simple Calculator");
+        JPanel panel = new JPanel();
+        JLabel inputLabel1 = new JLabel("Enter a number: ");
+        JLabel inputLabel2 = new JLabel("Enter a number: ");
+        inputField1 = new JTextField(10);
+        inputField2 = new JTextField(10);
+
+        JButton addBut = new JButton("Add");
+        JButton subtractBut = new JButton("Subtract");
+        JButton multBut = new JButton("Multiply");
+        JButton divBut = new JButton("Divide");
+
+        resultLabel = new JLabel("0.00");
+
+        window.setSize(200, 400);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+       addBut.addActionListener(new addListner());
+
+
+
+
+        panel.add(inputLabel1);
+        panel.add(inputField1);
+        panel.add(inputLabel2);
+        panel.add(inputField2);
+
+        panel.add(resultLabel);
+
+        panel.add(subtractBut);
+        panel.add(addBut);
+        panel.add(multBut);
+        panel.add(divBut);
+
+        window.add(panel);
+        window.setVisible(true);
+    }
+    private static class addListner implements ActionListener{
+    /*
+    1. Get Input - from the Text Fields
+    2. Add the numbers
+    3. Output to the result Label
+     */
+    public void actionPerformed(ActionEvent actionEvent) {
+        double number1 = Double.parseDouble(inputField1.getText());
+        double number2 = Double.parseDouble((inputField2.getText()));
+
+        resultLabel.setText(String.valueOf(number1 + number2));
+    }
+
+
+
+
+
+
+    }
 }
+
+
+
+
+
